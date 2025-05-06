4da6656f378b6fb2092931f7c2dd85c1d27de26b
molotkova_s <molotkova548@gmail.com>
2025-05-06 10:46:17 +0200

    adapted paths to cluster

--- cluster_submission/test_stygan2_execute.sh
+++ cluster_submission/test_stygan2_execute.sh
@@ -6,8 +6,8 @@ unset CONDA_PREFIX
 unset CONDA_DEFAULT_ENV
 unset CONDA_PYTHON_EXE
 
-path=/home/molotkova_s/Desktop/mlteam/posthoc-generative-cbm
-python_exec=/home/molotkova_s/miniforge3/envs/posthocgencbm/bin/python
+path=~/posthoc-generative-cbm
+python_exec=~/miniconda3/envs/posthocgencbm/bin/python
 cd "${path}"
 
 pwd=$(pwd)
@@ -15,4 +15,4 @@ export HOME="$(pwd)"
 export PYTHONPATH="${path}"
 export WANDB_API_KEY=""
 
-${python_exec} eval/test_stygan2_cpu.py
+${python_exec} eval/test_stygan2.py
