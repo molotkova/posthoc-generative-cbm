8fe017a3a46e85107f525cb0a3ffaec4a27a8c89
molotkova_s <molotkova548@gmail.com>
2025-05-11 17:03:08 +0200

    fixed test executable script - can now run on gpu

--- cluster_submission/test_stygan2_execute.sh
+++ cluster_submission/test_stygan2_execute.sh
@@ -6,13 +6,32 @@ unset CONDA_PREFIX
 unset CONDA_DEFAULT_ENV
 unset CONDA_PYTHON_EXE
 
-path=~/posthoc-generative-cbm
-python_exec=~/miniconda3/envs/posthocgencbm/bin/python
-cd "${path}"
+# Set torch visible gpus environment variable
+if command -v nvidia-smi &> /dev/null; then
+    GPU_COUNT=$(nvidia-smi --list-gpus | wc -l)
+    if [ "$GPU_COUNT" -gt 0 ]; then
+        echo "Detected $GPU_COUNT GPUs"
+        # Create a comma-separated list of all GPU indices
+        GPU_INDICES=$(seq -s, 0 $((GPU_COUNT-1)))
+        export CUDA_VISIBLE_DEVICES=$GPU_INDICES
+        echo "Setting CUDA_VISIBLE_DEVICES=$GPU_INDICES"
+    else
+        echo "No GPUs detected"
+    fi
+else
+    echo "nvidia-smi not found, assuming no GPUs available"
+fi
 
+path=/home/somo00003/posthoc-generative-cbm  # Project working directory
+
+eval "$(~/miniconda3/bin/conda shell.bash hook)"
+conda activate posthocgencbm
+
+cd "${path}"
 pwd=$(pwd)
+
 export HOME="$(pwd)"
 export PYTHONPATH="${path}"
 export WANDB_API_KEY=""
 
-${python_exec} eval/test_stygan2.py
+python eval/test_stygan2.py
