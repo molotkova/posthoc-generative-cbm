a50095b677cae9fb60bf8dca48804ed936d7f130
molotkova_s <molotkova548@gmail.com>
2025-07-08 13:42:48 +0200

    removed redundancy in output files naming

--- scripts/train_cbae.sh
+++ scripts/train_cbae.sh
@@ -16,7 +16,7 @@ if [ -n "$out_dir" ]; then
 fi
 
 ### training CB-AE for CelebA-HQ-pretrained StyleGAN2 with supervised models as pseudolabeler
-python -u train/train_cbae_gan.py -d celebahq40 -e cbae_stygan2_thr90 -p supervised -s 40concepts $OUT_ARG
+python -u train/train_cbae_gan.py -d celebahq40 -e cbae_stygan2_thr90 -p supervised $OUT_ARG
 
 ### training CC for CelebA-HQ-pretrained StyleGAN2 with supervised models as pseudolabeler
 # python3 -u train/train_cc_gan.py -e cc_stygan2_thr90 -p supervised -t sup_pl_cls8
